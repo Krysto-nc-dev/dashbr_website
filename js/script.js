9a4bf4031da734ab63061a52bc8af8cc868b84fc
Velten Stoyann <krysto.dev@gmail.com>
2024-07-02 06:04:57 +1100

    chapter and summary section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// toggle mobile menu
+
 document.addEventListener('DOMContentLoaded', function () {
   // Get the input element and button element
   const toggleButton = document.querySelector('.navbar .mobile-menu-toggle')
@@ -8,4 +10,17 @@ document.addEventListener('DOMContentLoaded', function () {
     // Toggle the display property of the mobile menu items
     mobileMenu.classList.toggle('active')
   })
+})
+
+// change navbar background on scroll
+
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar')
+
+  // Check if the scroll position is greater than 0
+  if (window.scrollY > 0) {
+    navbar.classList.add('navbar-scroll')
+  } else {
+    navbar.classList.remove('navbar-scroll')
+  }
 })
